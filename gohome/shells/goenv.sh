a2507ad94a7c59a94cd6e13e14d449f03780564f
文余两人 <523272086@qq.com>
2019-03-08 17:41:19 +0800

    upload goenv.sh
    用于构建环境变量

---
+++ gohome/shells/goenv.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# 建立内部变量和输出路径
+export GOHOME=`dirname ${MYLODER}`
+export GOROOT=${GOHOME}/go
+export GOPATH=${GOHOME}/gopath
+export PATH=${GOROOT}/bin:${GOPATH}/bin:${PATH}
+
+echo "GOHOME=${GOHOME}"
+echo "GOROOT=${GOROOT}"
+echo "GOPATH=${GOPATH}"
+echo "PATH=${PATH}"
+
