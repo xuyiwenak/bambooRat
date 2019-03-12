b9e4da49e790f11984c4084d87a3f72497a6769c
文余两人 <523272086@qq.com>
2019-03-12 18:20:03 +0800

    根据dep适配框架

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ set -e
 MYLODER=$(cd `dirname ${0}`; pwd)
 cd ${MYLODER}
 # 先设置环境变量
-source ${MYLODER}/goenv.sh
+source ${MYLODER}/tools/goenv.sh
 
 set +e
 echo "Clean old content ..."
@@ -16,12 +16,8 @@ set -e
 # 解压缩go执行文件
 cd ${GOHOME}
 curl -L https://studygolang.com/dl/golang/go1.12.src.tar.gz -o temp.tar
-tar xf temp.tar 
+tar xvf temp.tar
 rm -rf temp.tar
-
-
-
-
 
 # 设置环境变量
 echo "update ~/.bash_profile content ..."
@@ -41,4 +37,5 @@ echo "export GOROOT=${GOROOT}"    >> ${PFILE}
 echo "export GOPATH=${GOPATH}"    >> ${PFILE}
 echo "export PATH=\${GOROOT}/bin:\${GOPATH}/bin:\${PATH}" >> ${PFILE}
 
-echo "OK Done !"
+echo "configure go env done !"
+
--- tools/goenv.sh
+++ tools/goenv.sh
@@ -3,7 +3,7 @@
 # 建立内部变量和输出路径
 export GOHOME=`dirname ${MYLODER}`
 export GOROOT=${GOHOME}/go
-export GOPATH=${GOHOME}/gopath
+export GOPATH=${GOHOME}/goprojects
 export PATH=${GOROOT}/bin:${GOPATH}/bin:${PATH}
 
 echo "GOHOME=${GOHOME}"
