a9f7a3d3127b17134012a46866f43ec5f4a9df41
文余两人 <523272086@qq.com>
2019-03-14 15:21:03 +0800

    修改本地判断逻辑

--- start.sh
+++ start.sh
@@ -8,6 +8,27 @@ source ${MYLODER}/tools/goenv.sh
 GOFOLDER="${MYLODER}/go"
 GOPROJECTS="${MYLODER}/goprojects"
 
+if [ -d ${GOFOLDER} ];
+then
+    echo "locate the same GOROOT in ${GOROOT}"
+    if [ -d ${GOPROJECTS} ];
+    then
+        echo "locate the same GOPATH in ${GOPATH}"
+        exit 1
+    else
+        rm -rf ${GOPATH}
+        echo "Clean old GOPATH ...Done"
+    fi
+else
+    rm -rf ${GOROOT}
+    echo "Clean old GOROOT ...Done"
+fi
+echo "configure go env done !"
+
+set -e
+# 安装go pkg
+source ${MYLODER}/tools/goinstall.sh
+
 # 设置环境变量
 echo "update ~/.bash_profile ..."
 PFILE="$HOME/.bash_profile"
@@ -28,27 +49,4 @@ echo "export PATH=\${GOROOT}/bin:\${GOPATH}/bin:\${PATH}" >> ${PFILE}
 source $PFILE
 echo "环境变量发生变化，你需要重新登录才可能生效"
 echo "configure go env done !"
-
-if [ ${GOROOT} == ${GOFOLDER} ];
-then
-    echo "locate the same GOROOT in ${GOROOT}"
-    if [ ${GOPATH} == ${GOPROJECTS} ];
-    then
-        echo "locate the same GOPATH in ${GOPATH}"
-        exit 1
-    else
-        rm -rf ${GOPATH}
-        echo "Clean old GOPATH ...Done"
-    fi
-else
-    rm -rf ${GOROOT}
-    echo "Clean old GOROOT ...Done"
-fi
-echo "configure go env done !"
-
-set -e
-# 安装go pkg
-source ${MYLODER}/tools/goinstall.sh
-
-
 
