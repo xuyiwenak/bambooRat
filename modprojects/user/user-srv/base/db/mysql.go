f3b3ff2ffbca130eecf2d260decf5c9611b02bb6
文余两人 <523272086@qq.com>
2019-05-13 15:11:57 +0800

    重新编排容器

--- modprojects/user/user-srv/base/db/mysql.go
+++ modprojects/user/user-srv/base/db/mysql.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"database/sql"
+	"fmt"
 	"github.com/micro/go-log"
 	"github.com/xuyiwenak/bambooRat/modprojects/user/user-srv/base/config"
 )
@@ -9,6 +10,8 @@ import (
 func initMysql() {
 
 	var err error
+
+	log.Log("sql--->", config.GetMysqlConfig().GetURL())
 
 	// 创建连接
 	mysqlDB, err = sql.Open("mysql", config.GetMysqlConfig().GetURL())
@@ -27,4 +30,5 @@ func initMysql() {
 	if err = mysqlDB.Ping(); err != nil {
 		log.Fatal(err)
 	}
+	log.Log("sql--->ok")
 }
