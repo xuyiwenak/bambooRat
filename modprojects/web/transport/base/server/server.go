9fea8348b01f0a02848f829abd1561d903aebe05
文余两人 <523272086@qq.com>
2019-08-13 21:10:26 +0800

    更新base用例

--- modprojects/web/transport/base/server/server.go
+++ modprojects/web/transport/base/server/server.go
@@ -7,70 +7,43 @@ import (
 
 //transport模块的接口主要是用于服务之间连接。它使用基于连接的socket send/recv语义和
 //有各种实现；http, grpc, memory, quic. 默认使用http.
+var (
+	addr = "127.0.0.1:54321"
+)
 
 func main() {
 	tr := transport.NewTransport()
 
-	l, err := tr.Listen("127.0.0.1:54321")
-	log.Logf("listening on port:%s ...", l.Addr())
+	l, err := tr.Listen(addr)
+	log.Logf("listening on port:%s ...", addr)
 	if err != nil {
 		log.Fatalf("Unexpected listen err: %v", err)
 	}
 	defer l.Close()
 
+	done := make(chan bool)
+	// socket func
 	fn := func(sock transport.Socket) {
 		defer sock.Close()
+
 		for {
-			var m transport.Message
-			if err := sock.Recv(&m); err != nil {
-				log.Fatalf("rev", err)
+			var msg transport.Message
+			if err := sock.Recv(&msg); err != nil {
 				return
 			}
-
-			if err := sock.Send(&m); err != nil {
-				log.Fatalf("send", err)
+			log.Logf("recv msg:%s", string(msg.Body))
+			if err := sock.Send(&msg); err != nil {
 				return
 			}
 		}
 	}
-
-	done := make(chan bool)
-
-	go func() {
-		if err := l.Accept(fn); err != nil {
-			select {
-			case <-done:
-			default:
-				log.Fatalf("Unexpected accept err: %v", err)
-			}
+	// 这里先阻塞，如果不想的话启动一个协程单独跑
+	if err := l.Accept(fn); err != nil {
+		select {
+		case <-done:
+		default:
+			log.Fatalf("Unexpected accept err: %v", err)
 		}
-	}()
-
-	c, err := tr.Dial(l.Addr())
-	if err != nil {
-		log.Fatalf("Unexpected dial err: %v", err)
-	}
-	defer c.Close()
-
-	m := transport.Message{
-		Header: map[string]string{
-			"Content-Type": "application/json",
-		},
-		Body: []byte(`{"message": "Hello World"}`),
-	}
-
-	if err := c.Send(&m); err != nil {
-		log.Fatalf("Unexpected send err: %v", err)
-	}
-
-	var rm transport.Message
-
-	if err := c.Recv(&rm); err != nil {
-		log.Fatalf("Unexpected recv err: %v", err)
-	}
-
-	if string(rm.Body) != string(m.Body) {
-		log.Fatalf("Expected %v, got %v", m.Body, rm.Body)
 	}
 	close(done)
 }
